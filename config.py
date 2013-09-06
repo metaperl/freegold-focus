b89afdbe829c9c47e5a892db0af79c22471db456
metaperl <metaperl@gmail.com>
2013-09-06 14:36:52 -0400

    looking very good now.

--- config.py
+++ config.py
@@ -1,45 +1,32 @@
 import os
 def full_path(p=""): return os.path.join(os.path.dirname(__file__), p)
 
+def config_static_directory(d):
+    return {
+        "/{0}".format(d) : {
+            'tools.staticdir.on'  : True,
+            'tools.staticdir.dir' : d
+        }
+    }
+
+def static_dirs():
+    _static_dirs = 'css img images js media'.split()
+    for static_dir in _static_dirs:
+        yield config_static_directory(static_dir)
+
+
 config = {
+
     'global': {
         'environment': 'embedded',
         'log.screen': True
     },
     '/': {
         'tools.staticdir.root' : full_path(),
-    },
-    '/css': {
-        'tools.staticdir.on' : True,
-        'tools.staticdir.dir' : 'css',
-    },
-    'css': {
-        'tools.staticdir.on' : True,
-        'tools.staticdir.dir' : 'css',
-    },
-    '/img': {
-        'tools.staticdir.on' : True,
-        'tools.staticdir.dir' : 'img',
-    },
-    '/images': {
-        'tools.staticdir.on' : True,
-        'tools.staticdir.dir' : 'images',
-    },
-    'images': {
-        'tools.staticdir.on' : True,
-        'tools.staticdir.dir' : 'images',
-    },
-    '/js': {
-        'tools.staticdir.on' : True,
-        'tools.staticdir.dir' : 'js',
-    },
-    'js': {
-        'tools.staticdir.on' : True,
-        'tools.staticdir.dir' : 'js',
-    },
-    '/media': {
-        'tools.staticdir.on' : True,
-        'tools.staticdir.dir': 'media'
     }
 
 }
+
+for static_dir in static_dirs():
+    config.update(static_dir)
+
--- myapp.py
+++ myapp.py
@@ -1,86 +1,117 @@
 
-# -*- python -*-
-
-# core
+# imports
+## core
 import importlib
 import os
 import sys
 import StringIO
 
-# 3rd party
+## 3rd party
 import cherrypy
 import meld3
 
-# local
+## local
 def full_path(*extra):
     return os.path.join(os.path.dirname(__file__), *extra)
 sys.path.insert(0, full_path())
 from saorm import *
 
 
-class Root(object):
+class AffiliateModel(object):
 
-    def person(self, kbid):
+    def __init__(self, kb_id):
+        self.kb_id=kb_id
+        self.affiliate = self.person()
+
+    def person(self):
         #example on how to query your Schema
         from sqlalchemy.orm import sessionmaker
         session = sessionmaker(bind=engine)()
-        p = session.query(Affiliate).filter_by(id=kbid).first()
+        p = session.query(Affiliate).filter_by(id=self.kb_id).first()
         return p
 
-    html_file = full_path('index.html')
+class AffiliatePage(object):
 
-    @cherrypy.expose
-    def index(self, s="supreme", cmpg=None, opener='corp'):
+    def __init__(self, kb_id, html_file='index.html'):
+        self.html_file = full_path(html_file)
+        self.p = AffiliateModel(kb_id).affiliate
+        self.affiliate_url = 'http://karatbars.com/signup.php?s={0}'.format(kb_id)
+        self.landing_url = 'http://karatbars.com/landing/?s={0}'.format(kb_id)
+        self.skype_url = 'skype:{0}?add'.format(self.p.skype)
+        self.kbgold_uk_url = 'http://www.karatbarsgold.co.uk/{0}'.format(self.p.kbuk_id)
+        self.email_href = 'mailto:{0}'.format(self.p.email)
 
-        p = self.person(kbid=s)
+    def render(self):
+        self.root.findmeld('affiliate_url').attributes(href=self.affiliate_url)
+        self.root.findmeld('kbgold_uk_url').attributes(href=self.kbgold_uk_url)
+        self.root.findmeld('enroll_free').attributes(href=self.affiliate_url)
+        self.root.findmeld('contact_iframe').attributes(src=self.landing_url)
+        self.root.findmeld('name').content(self.p.name)
+        self.root.findmeld('name_in_title').content(
+            "Karatbars International - {0}".format(self.p.name)
+        )
+
+        self.root.findmeld('pic').attributes(src=self.p.pic)
+        self.root.findmeld('phone').content(self.p.number)
+        self.root.findmeld('skype_url').attributes(href=self.skype_url)
+        self.root.findmeld('skype_id').content(self.p.skype)
+        self.root.findmeld('email_href').attributes(href=self.email_href)
+        self.root.findmeld('email').content(self.p.email)
+
+
+class Reese(AffiliatePage):
+
+    def __init__(self, kb_id, opener):
+        self.opener = opener
+        super(Reese, self).__init__(kb_id)
+
+    def render(self):
+
+        super(Reese, self).render()
 
         src = {
             'corp': 'http://www.youtube.com/embed/0lrqEGlu0Fo',
             'uk': 'http://www.youtube.com/embed/30MfCTLhdZ4',
+            'selina' : 'http://www.youtube.com/embed/37l6Wdzw490'
         }
 
-        followers = {
-            'corp': 'uk',
-            'uk': 'corp',
-        }
+        def lead_and_followers(lead):
+            followers = src.keys()
+            followers.remove(lead)
+            return (lead, followers)
+
+        followers = dict(lead_and_followers(l) for l in src.keys())
 
         carousel = dict(
-            opener=src[opener],
-            follower = src[followers[opener]]
+            opener=src[self.opener],
+            follower = src[followers[self.opener][0]],
+            follower2 = src[followers[self.opener][1]]
+
         )
 
-        affiliate_url = 'http://karatbars.com/signup.php?s={0}'.format(s)
-        landing_url = 'http://karatbars.com/landing/?s={0}'.format(s)
-        skype_url = 'skype:{0}?add'.format(p.skype)
-        affiliate_url = 'http://karatbars.com/signup.php?s={0}'.format(s)
-        kbgold_uk_url = 'http://www.karatbarsgold.co.uk/{0}'.format(p.kbuk_id)
-        #slide = '{0}?autoplay=1'.format(default_slide)
-        email_href = 'mailto:{0}'.format(p.email)
+        def autoplay(u): return '{0}?autoplay=1'.format(u)
 
-        with open(self.html_file, 'r') as fp:
+        for meld_id, url in carousel.iteritems():
+            self.root.findmeld(meld_id).attributes(src=autoplay(url))
+
+
+class Root(object):
+
+    @cherrypy.expose
+    def index(self, s="supreme", page='Reese', opener='selina', cmpg=None, banner=None):
+
+        module = __import__(__name__)
+        class_ = getattr(module, page)
+        if page == 'Reese':
+            affiliate_page = class_(s, opener)
+        else:
+            affiliate_page = class_(s)
+
+        with open(affiliate_page.html_file, 'r') as fp:
             html = fp.read()
-        root = meld3.parse_htmlstring(html)
-        for lookup, src in carousel.iteritems():
-            root.findmeld(lookup).attributes(src='{0}?autoplay=1'.format(src))
-        root.findmeld('affiliate_url').attributes(href=affiliate_url)
-        root.findmeld('kbgold_uk_url').attributes(href=kbgold_uk_url)
-        root.findmeld('enroll_free').attributes(href=affiliate_url)
-        root.findmeld('contact_iframe').attributes(src=landing_url)
-        root.findmeld('name').content(p.name)
-        root.findmeld('name_in_title').content(
-            "Karatbars International - {0}".format(p.name)
-        )
 
-        root.findmeld('pic').attributes(src=p.pic)
-        root.findmeld('phone').content(p.number)
-        root.findmeld('skype_url').attributes(href=skype_url)
-        root.findmeld('skype_id').content(p.skype)
-        # iterator = root.findmeld('numbers').repeat(u['numbers'])
-        # for elem, item in iterator:
-        #     elem.findmeld('number').content(item)
-        #     #root.write_xhtml(output)
-        root.findmeld('email_href').attributes(href=email_href)
-        root.findmeld('email').content(p.email)
+        affiliate_page.root = meld3.parse_htmlstring(html)
+        affiliate_page.render()
 
-        return root.write_htmlstring()
+        return affiliate_page.root.write_htmlstring()
 
