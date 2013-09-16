64fd006c4f8a66467074e05be2d716f51e6ca6db
metaperl <metaperl@gmail.com>
2013-09-15 21:55:45 -0400

    mentor files

---
+++ trainwith/js/scripts.js
@@ -0,0 +1,23 @@
+$(function() {
+	$('ul.nav a').bind('click',function(event){
+		var $anchor = $(this);
+                    
+		$('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500,'easeInOutExpo');
+        /*
+        if you don't want to use the easing effects:
+        $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000);
+        */
+        event.preventDefault();
+	});
+});
+
+$(function(){
+    $('.sectionwrapper') .css({'height': (($(window).height()))+'px'});
+    $(window).resize(function(){
+        $('.sectionwrapper') .css({'height': (($(window).height()))+'px'});
+    });
+});
