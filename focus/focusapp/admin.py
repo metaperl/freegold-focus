e42886f6efa95dee5943abd3d3b8b62267601cf5
Terrence Brannon <metaperl@gmail.com>
2014-02-09 02:58:09 -0500

    fix

--- focus/focusapp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from focusapp.models import Person
-
-admin.site.register(Person)
--- person/flash73.py
+++
@@ -1,9 +0,0 @@
-user = dict(
-    name="John Fawcett, Sr.",
-    numbers = [
-        "425-339-9368 - office"
-    ],
-    email = "johnfawcettsr@gmail.com",
-    skype = "metaperl",
-    pic = "https://lh3.googleusercontent.com/-c-akau9O4Qk/AAAAAAAAAAI/AAAAAAAAAAA/ASiO13pkQ5E/s120-c/photo.jpg"
-)
