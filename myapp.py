328b577dbd486cc032075ecf7cfbb8924d379896
Terrence Brannon <metaperl@gmail.com>
2015-02-02 09:00:27 +0000

    text encoding in meld3 is brittle

--- myapp.py
+++ myapp.py
@@ -10,12 +10,15 @@ import StringIO
 
 ## 3rd party
 import cherrypy
+sys.path.insert(0, "/home/schemelab/prg/meld3")
 import meld3
 
 ## local
 def full_path(*extra):
     return os.path.join(os.path.dirname(__file__), *extra)
 sys.path.insert(0, full_path())
+
+
 import email_rst
 from saorm import *
 
